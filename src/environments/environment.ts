09f17ebb43f7b27c96a635a15ab8e9aed87e1a61
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-01-28 17:30:29 -0600

    cambio de enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://ict-custom-inbox-chemistry.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://happening-engage-demo-bad.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
