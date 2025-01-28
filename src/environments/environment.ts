008dad21443bfc2bdfd884d03d29e6006a9ba3e8
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-01-28 15:37:52 -0600

    cambio en enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://ict-custom-inbox-chemistry.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
