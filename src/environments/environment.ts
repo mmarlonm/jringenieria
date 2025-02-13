5f180c1c3fa0e2f97fec02055085d2f596c1f0b3
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-02-13 10:26:38 -0600

    actualizar URL de API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: ' https://calcium-five-limits-highlight.trycloudflare.com'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
