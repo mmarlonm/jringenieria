ad4117adb7d6fb209fa640c40b22d72f8677dcd9
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-02-13 10:28:40 -0600

    ajustar formato de URL de API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: ' https://calcium-five-limits-highlight.trycloudflare.com'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://calcium-five-limits-highlight.trycloudflare.com'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
