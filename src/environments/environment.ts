e46c2fdb14c4321dba7f104d502413d781235a87
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-02 20:41:22 -0600

    actualizar URL de API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'http://3.84.194.49/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'https://basis-med-bali-sep.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
