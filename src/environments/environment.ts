13480238ff34b8aea1a9ba618751730b21700bb6
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-04 09:32:56 -0600

    actualizar URL de API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://ignored-neighborhood-sight-mold.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://continental-moore-says-you.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
