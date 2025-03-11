9780708802f9b8ed53bf0e7186b3fe7998a1ecff
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-11 11:01:06 -0600

    actualizar URL de API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://distributors-orbit-toner-confident.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://bali-nurses-occupations-plastics.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
