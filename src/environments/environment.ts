d6b1acf3389af4cfbfb1cfec7ecf999b611e2f78
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-13 14:37:32 -0600

    actualizar la URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://images-wish-covered-auction.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://classics-benefit-pregnant-invited.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
