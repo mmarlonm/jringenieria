3fb4be437ea2af86d1f4ceda2cad7f603ad1662b
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-14 08:03:03 -0600

    actualizar la URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    //apiUrl: 'https://uses-algorithms-counties-pens.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    apiUrl: 'https://gr-oo-lender-upload.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
   };
