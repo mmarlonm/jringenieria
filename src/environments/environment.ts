f818295a4bb486491d717857b3126e7c8893b77d
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-21 08:48:37 -0600

    actualizar URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://ease-feature-leeds-gibson.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrl: 'https://optional-locator-specialized-magazines.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
   };
