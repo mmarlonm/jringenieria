49ff1efd640be5865eb3d8010775b89cf611ac73
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-03-26 09:21:16 -0600

    actualizar URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://rainbow-calculations-aim-site.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrl: 'https://scholarships-exclusively-blend-hl.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
   };
