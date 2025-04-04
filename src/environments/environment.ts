d694c80e6989f791b1fa9a635730e21515124e41
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-04-04 09:11:24 -0600

    actualizar la URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
+    //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    //apiUrl: 'https://logged-christina-eds-authors.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrl: 'https://chemicals-doctrine-hang-riders.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
   };
