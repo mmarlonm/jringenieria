e8bf56e00ded28b43fa6410c266060ce82967bdf
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-04-10 20:45:42 -0600

    fix: actualizar la URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:5162/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://shine-smilies-ls-arrange.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrl: 'https://update-senators-analyst-month.trycloudflare.com/api'  // Aquí va la URL de tu API (ajusta según sea necesario) 
   };
