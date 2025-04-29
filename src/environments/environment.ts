8900b5cb737b65d2d22c0a498379124f6f42d9cd
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-04-29 10:07:37 -0600

    fix: actualizar las URLs de la API a la dirección correcta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://princess-vacation-vault-findarticles.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://princess-vacation-vault-findarticles.trycloudflare.com'
+    apiUrl: 'https://crude-enemies-became-catalogue.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://crude-enemies-became-catalogue.trycloudflare.com'
   };
