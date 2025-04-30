7cc355dc32f7b6260e9a9dda2e1a099d4db73a1d
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-04-30 17:23:48 -0600

    fix: actualizar las URLs de la API a la nueva dirección

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://within-trembl-commander-fuji.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://within-trembl-commander-fuji.trycloudflare.com'
+    apiUrl: 'https://tomatoes-recipe-graham-divisions.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://tomatoes-recipe-graham-divisions.trycloudflare.com'
   };
