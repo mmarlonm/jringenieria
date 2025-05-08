281ea0ac1b89caf7080251f77e30851caeeb2113
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-05-08 09:44:06 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://douglas-touring-wind-discount.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://douglas-touring-wind-discount.trycloudflare.com'
+    apiUrl: 'https://portions-fellow-baths-release.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://portions-fellow-baths-release.trycloudflare.com'
   };
