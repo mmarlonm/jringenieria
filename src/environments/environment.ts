22ca04648ed6998675de433132f22466bc16580d
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-05-12 09:05:31 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://desperate-becoming-lips-dialog.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://desperate-becoming-lips-dialog.trycloudflare.com'
+    apiUrl: 'https://operated-poor-ripe-future.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://operated-poor-ripe-future.trycloudflare.com'
   };
