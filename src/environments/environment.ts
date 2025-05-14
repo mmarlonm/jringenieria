120a4086b3143301c8633a7291d92738c4b6008f
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-05-13 19:14:40 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://loss-advertise-eau-amino.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://loss-advertise-eau-amino.trycloudflare.com'
+    apiUrl: 'https://hist-tahoe-additional-conclusions.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://hist-tahoe-additional-conclusions.trycloudflare.com'
   };
