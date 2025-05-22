7e768998d06945630f255ae9109aae2abd08c3c3
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-05-22 17:48:58 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://lace-slot-oils-flat.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://lace-slot-oils-flat.trycloudflare.com'
+    apiUrl: 'https://bryan-signatures-cant-referenced.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://bryan-signatures-cant-referenced.trycloudflare.com'
   };
