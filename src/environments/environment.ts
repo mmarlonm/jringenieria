11b4ea9a3945b36bd16f8f504698423b55922ba0
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-05-30 14:00:26 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://spam-grip-researchers-fixed.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://spam-grip-researchers-fixed.trycloudflare.com'
+    apiUrl: ' https://electoral-milan-sam-different.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: ' https://electoral-milan-sam-different.trycloudflare.com'
   };
