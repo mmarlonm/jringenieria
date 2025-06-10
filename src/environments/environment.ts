0fd3337c607ef276d367f3ef61d086b5c6fc5d48
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-06-10 09:59:40 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://films-evaluate-pulled-turkey.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://films-evaluate-pulled-turkey.trycloudflare.com'
+    apiUrl: 'https://plaza-eternal-linux-ping.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://plaza-eternal-linux-ping.trycloudflare.com'
   };
