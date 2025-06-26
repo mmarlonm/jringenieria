6cdd4d295096227f34ac95537d70bb86d028ee00
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-06-26 08:12:56 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrlSignal: 'http://localhost:5162'  // chat real time
+    //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
+    //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    //apiUrl: 'https://civilian-specialists-rabbit-ratings.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    //apiUrlSignal: 'https://civilian-specialists-rabbit-ratings.trycloudflare.com'
+    apiUrl: 'https://trees-morrison-pc-endangered.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://trees-morrison-pc-endangered.trycloudflare.com'
   };
