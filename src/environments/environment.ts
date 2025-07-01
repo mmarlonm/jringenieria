876d42d9e9880ae542f569ae224a9b2bf5747720
c.marlgm <marlon93gm@gmail.com>
2025-07-01 11:27:41 -0600

    fix: actualizar las URLs de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrl: 'https://se-pediatric-kenny-webpage.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-    apiUrlSignal: 'https://se-pediatric-kenny-webpage.trycloudflare.com'
+    apiUrl: 'https://ann-housing-tags-casey.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+    apiUrlSignal: 'https://ann-housing-tags-casey.trycloudflare.com'
   };
