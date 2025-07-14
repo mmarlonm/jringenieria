79d23712382639f68391eafcd53eb26745b3abbf
c.marlgm <marlon93gm@gmail.com>
2025-07-14 03:25:15 -0600

    fix: update API URLs to use the correct production endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
-    apiUrlSignal: 'http://localhost:5162'  // chat real time
+    //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
+    //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   //apiUrl: 'https://entrepreneurs-bucks-arts-tgp.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   //apiUrlSignal: 'https://entrepreneurs-bucks-arts-tgp.trycloudflare.com'
+   apiUrl: 'https://complement-cooler-uw-cosmetic.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://complement-cooler-uw-cosmetic.trycloudflare.com'
   };
