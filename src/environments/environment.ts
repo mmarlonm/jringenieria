15331f22249f3c094127d7a57cb088a25bc040ef
c.marlgm <marlon93gm@gmail.com>
2025-07-19 11:19:02 -0600

    fix: update API URLs to the correct endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: 'https://appearance-inflation-recommends-david.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: 'https://appearance-inflation-recommends-david.trycloudflare.com'
+   apiUrl: 'https://chance-robots-brakes-ate.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://chance-robots-brakes-ate.trycloudflare.com'
   };
