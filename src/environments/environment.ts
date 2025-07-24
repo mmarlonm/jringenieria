76e5c4d4f33912913387b350d761a70c536c57f9
c.marlgm <marlon93gm@gmail.com>
2025-07-24 10:43:00 -0600

    fix: update API URLs to the correct endpoints in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: 'https://io-compaq-rd-household.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: 'https://io-compaq-rd-household.trycloudflare.com'
+   apiUrl: 'https://jennifer-separated-environment-household.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://jennifer-separated-environment-household.trycloudflare.com'
   };
