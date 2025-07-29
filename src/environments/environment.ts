e80514b89a7f14225b2facd659aebe49687810f1
c.marlgm <marlon93gm@gmail.com>
2025-07-29 09:31:13 -0600

    fix: update API URLs to the correct endpoints in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: 'https://creek-remain-clinton-speak.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: 'https://creek-remain-clinton-speak.trycloudflare.com'
+   apiUrl: 'https://checklist-nationally-diamonds-reconstruction.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://checklist-nationally-diamonds-reconstruction.trycloudflare.com'
   };
