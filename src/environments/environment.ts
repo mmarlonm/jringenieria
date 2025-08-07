6ecb7ac12fd510fe38abb6862d1021a04693ef24
c.marlgm <marlon93gm@gmail.com>
2025-08-07 09:33:08 -0600

    url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: 'https://protocols-whats-switching-determined.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: 'https://protocols-whats-switching-determined.trycloudflare.com'
+   apiUrl: 'https://many-cleaners-hz-granny.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://many-cleaners-hz-granny.trycloudflare.com'
   };
