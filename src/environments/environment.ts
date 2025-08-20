d3c9ed923fe4b008b85074d822cbf684a32b936e
c.marlgm <marlon93gm@gmail.com>
2025-08-20 09:01:04 -0600

    fix(environment): update API URLs for consistency

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: 'https://tuesday-rim-dream-against.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: 'https://tuesday-rim-dream-against.trycloudflare.com'
+   apiUrl: 'https://engaged-tactics-medication-middle.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://engaged-tactics-medication-middle.trycloudflare.com'
   };
