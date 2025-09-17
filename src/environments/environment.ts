6c425da7a05d1112365c60252ea74fb3b9ad5b0f
c.marlgm <marlon93gm@gmail.com>
2025-09-17 07:57:52 -0600

    url prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: ' https://olympic-lol-benz-subjective.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: ' https://olympic-lol-benz-subjective.trycloudflare.com'
+   apiUrl: 'https://wayne-selective-genetic-setup.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://wayne-selective-genetic-setup.trycloudflare.com'
   };
