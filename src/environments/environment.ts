dc0fb85d5f9dfe2b98b9e4af2c9b3b84bd5add23
c.marlgm <marlon93gm@gmail.com>
2025-10-30 08:28:51 -0600

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@ export const environment = {
     //apiUrl: 'http://localhost:5162/api',  // Aquí va la URL de tu API (ajusta según sea necesario)
     //apiUrlSignal: 'http://localhost:5162'  // chat real time
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
-   apiUrl: 'https://lopez-prime-promptly-longer.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
-   apiUrlSignal: 'https://lopez-prime-promptly-longer.trycloudflare.com',
+   apiUrl: 'https://smoke-batteries-contributing-keys.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
+   apiUrlSignal: 'https://smoke-batteries-contributing-keys.trycloudflare.com',
+   apiOnlyOffice: 'http://host.docker.internal:8080/',
    apiKeyGoogle: 'AIzaSyCnhkYFNO57qkBrvOaIFJwZy6vDYtJMncg'
   };
