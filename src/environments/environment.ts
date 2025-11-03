4f84966fa95f93edbb948a1da1bb30a8bf343840
c.marlgm <marlon93gm@gmail.com>
2025-11-03 11:46:39 -0600

    url prod doc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
     //apiUrl: 'http://54.85.248.65/api'  // Aquí va la URL de tu API (ajusta según sea necesario)
    apiUrl: 'https://outputs-struck-sea-local.trycloudflare.com/api',  // Aquí va la URL de tu API (ajusta según sea necesario) 
    apiUrlSignal: 'https://outputs-struck-sea-local.trycloudflare.com',
-   apiOnlyOffice: 'https://streams-sara-affecting-float.trycloudflare.com',
+   apiOnlyOffice: 'https://influences-targeted-oem-airlines.trycloudflare.com',
    apiKeyGoogle: 'AIzaSyCnhkYFNO57qkBrvOaIFJwZy6vDYtJMncg'
   };
