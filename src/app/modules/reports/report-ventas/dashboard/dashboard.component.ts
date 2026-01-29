9e4dd39bf539846b66e61be9415c43ad6c487fbd
c.marlgm <marlon93gm@gmail.com>
2026-01-29 16:07:04 -0600

    cambios sucursal

--- src/app/modules/reports/report-ventas/dashboard/dashboard.component.ts
+++ src/app/modules/reports/report-ventas/dashboard/dashboard.component.ts
@@ -24,7 +24,7 @@ export class ReportVentasDashboardComponent implements OnInit {
     Highcharts: typeof Highcharts = Highcharts;
 
     // 🔹 Filtros
-    sucursal: string = 'Factura PACHUCA';
+    sucursal: string = 'PACHUCA';
     fechaInicio!: string;
     fechaFin!: string;
 
